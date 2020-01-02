37084cd0b95519fb1ae8e335c3036d62dc4e896f
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 14:55:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  l = array.size
+  
+  # counter = 0
+  # while counter <  l 
+  #   array[counter] = array[counter]*array[counter]
+  #   counter +=1
+  # end 
+  # or
+  
+  for i in 0...l 
+    array[i] = array[i]*array[i]
+  end 
+  array
 end
